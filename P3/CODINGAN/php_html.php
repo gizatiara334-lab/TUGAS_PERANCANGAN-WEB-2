fbb27e38a03d10a77610c159e729a3dc8461bc88
gizatiara334-lab <gizatiara334@gmail.com>
2025-10-20 20:55:20 +0700

    Create php_html.php

---
+++ P3/CODINGAN/php_html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Demo PHP di HTML</title>
+</head>
+<body>
+
+<p>Kode <?php echo "PHP"; ?> di HTML</p>
+
+<p>
+    <?php
+    // Menampilkan tanggal hari ini
+    date_default_timezone_set('Asia/Jakarta'); // set zona waktu Indonesia
+    echo "Tanggal hari ini: " . date("l, d F Y") . "<br>";
+    echo "Nama saya adalah Giza Aurelya<br>";
+    echo "NIM: 2402032<br>";
+    echo "Terima kasih semuanya 😊";
+    ?>
+</p>
+
+</body>
+</html>
