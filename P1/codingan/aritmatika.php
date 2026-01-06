548135362cb1e44f79548f36b7f432aa1d867374
gizatiara334-lab <gizatiara334@gmail.com>
2026-01-06 21:57:03 +0700

    Create aritmatika.php

---
+++ P1/codingan/aritmatika.php
@@ -0,0 +1,13 @@
+<?php
+$a = 10;
+$b = 3;
+
+echo "Nilai a = $a<br>";
+echo "Nilai b = $b<br><br>";
+
+echo "a + b = " . ($a + $b) . "<br>";
+echo "a - b = " . ($a - $b) . "<br>";
+echo "a * b = " . ($a * $b) . "<br>";
+echo "a / b = " . ($a / $b) . "<br>";
+echo "a % b = " . ($a % $b) . "<br>";
+?>
